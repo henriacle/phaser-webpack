e019f19bbd47e52bae5a31848be3e5a010c175ff
Henrique <henrique.clementino@gps-pamcary.com.br>
2018-05-28 21:57:06 -0300

    Adicionado mais metodos para deixar a create function mais limpa

--- src/game.ts
+++ src/game.ts
@@ -42,53 +42,12 @@ export namespace SimpleGame {
             this.cursors = this.input.keyboard.createCursorKeys();
             this.add.sprite(400,300, 'sky');
             this.bombs = this.physics.add.group();
-            this.platforms = this.physics.add.staticGroup();
-            this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-            this.platforms.create(600, 400, 'ground');
-            this.platforms.create(50, 250, 'ground');
-            this.platforms.create(750, 220, 'ground');
-
-            this.player = this.physics.add.sprite(100, 450, 'dude');
-
-            this.player.setBounce(0.2);
-            this.player.setCollideWorldBounds(true);
-
-            this.anims.create({
-                key: 'left',
-                frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
-                frameRate: 10,
-                repeat: -1
-            });
-
-            this.anims.create({
-                key: 'turn',
-                frames: [{key: 'dude', frame: 4}],
-                frameRate: 20
-            });
-
-            this.anims.create({
-                key: 'right',
-                frames: this.anims.generateFrameNames('dude', {start: 5, end: 8}),
-                frameRate: 10,
-                repeat: -1
-            });
-
-            this.stars = this.physics.add.group({
-                key: 'star',
-                repeat: 11,
-                setXY: {x: 12, y: 0, stepX: 70}
-            });
-
-            this.stars.children.iterate(function(child) {
-                child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-            }, this);
-
+            this.createPlatforms();
+            this.createPlayer();
+            this.createAnimations();
+            this.createStars();
             this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-
-            this.physics.add.collider(this.player, this.platforms);
-            this.physics.add.collider(this.stars, this.platforms);
-            this.physics.add.collider(this.bombs, this.platforms);
-            this.physics.add.collider(this.player, this.bombs, this.hitBomb, null, this);            
+            this.createColliders();
             this.physics.add.overlap(this.player, this.stars, this.collectStars, null, this);
         }
     
@@ -107,6 +66,20 @@ export namespace SimpleGame {
             if (this.cursors.up.isDown && this.player.body.touching.down) {
                 this.player.setVelocityY(-330);
             }
+        }
+
+        createPlayer() {
+            this.player = this.physics.add.sprite(100, 450, 'dude');
+            this.player.setBounce(0.2);
+            this.player.setCollideWorldBounds(true);            
+        }
+
+        createPlatforms() {
+            this.platforms = this.physics.add.staticGroup();
+            this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+            this.platforms.create(600, 400, 'ground');
+            this.platforms.create(50, 250, 'ground');
+            this.platforms.create(750, 220, 'ground');            
         }
 
             
@@ -130,11 +103,52 @@ export namespace SimpleGame {
             }
         }
 
+        createColliders() {
+            this.physics.add.collider(this.player, this.platforms);
+            this.physics.add.collider(this.stars, this.platforms);
+            this.physics.add.collider(this.bombs, this.platforms);
+            this.physics.add.collider(this.player, this.bombs, this.hitBomb, null, this);            
+        }
+
         hitBomb(player: Phaser.Physics.Arcade.Sprite, bomb: Phaser.Physics.Arcade.Sprite) {
             this.physics.pause();
             this.player.setTint(0xff0000);
             this.player.anims.play('turn');
             this.gameover = true;
+        }
+
+        createStars() {
+            this.stars = this.physics.add.group({
+                key: 'star',
+                repeat: 11,
+                setXY: {x: 12, y: 0, stepX: 70}
+            });
+
+            this.stars.children.iterate(function(child) {
+                child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+            }, this);            
+        }
+
+        createAnimations() {
+            this.anims.create({
+                key: 'left',
+                frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
+                frameRate: 10,
+                repeat: -1
+            });
+
+            this.anims.create({
+                key: 'turn',
+                frames: [{key: 'dude', frame: 4}],
+                frameRate: 20
+            });
+
+            this.anims.create({
+                key: 'right',
+                frames: this.anims.generateFrameNames('dude', {start: 5, end: 8}),
+                frameRate: 10,
+                repeat: -1
+            });            
         }
     }    
 }
